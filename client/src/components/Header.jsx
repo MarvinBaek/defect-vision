f9d225b7899fa4192414bebb3c6a6e93ce305d26
mkjun2504 <mkjun2504@gmail.com>
2025-05-26 16:39:27 +0900

    add: 헤더 변경, 드랍존 비디오 허용 변경, TypeButton 추가

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import markcloud from '../assets/markcloud.png';
+import { useImage } from '../context/ImageContext';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const { setImage } = useImage();
+
+  return (
+    <header>
+      <img
+        src={markcloud}
+        width={'120px'}
+        onClick={() => {
+          setImage([]);
+          navigate('/');
+        }}
+        style={{ cursor: 'pointer' }}
+      />
+      <span>부품 불량 판별 솔루션 </span>
+    </header>
+  );
+};
+
+export default Header;
