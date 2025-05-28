23f1110507cc57d09a769f50d9c9fced10bbd9ff
Mnam9058 <qoraudska97@gmail.com>
2025-05-28 11:44:41 +0900

    feat: CCTV 스키마 추가

--- server/schemas/predict_schema.py
+++ server/schemas/predict_schema.py
@@ -1,6 +1,19 @@
 from pydantic import BaseModel
-
+from typing import List
 
 class PredictResponse(BaseModel):
     resultImage: str  # base64
     metadata: dict    # {"status": bool, "percentage": float}
+
+class FileNameRequest(BaseModel):
+    filename: str
+
+class FileListResponse(BaseModel):
+    files: list[str]
+
+class MessageResponse(BaseModel):
+    message: str
+
+class PredictStatusResponse(BaseModel):
+    done: bool
+    logs: List[List[float]]
